1e2f7034b8326852b9967c82479d0ba982748b44
TatoExp <mwmatthew10@gmail.com>
2021-05-24 15:33:41 +0100

    added expresssharp

---
+++ csharp/express/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ExpressSharp;
+
+namespace Expr
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Express server = new Express();
+
+			server.GET("/", (req, res) => res.Status(200));
+
+			server.GET("/user/{id}", (req, res) =>
+			{
+				res.Send(req._request.Url.Segments[1].Substring(1));
+			});
+
+			server.POST("/", (req, res) => res.Status(200));
+
+			server.Listen(80);
+		}
+	}
+}
